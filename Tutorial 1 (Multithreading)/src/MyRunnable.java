bf2c837b5ad192357293f6fe2cbee018e0e5e6da
Mehrdad <muadibbm@yahoo.com>
2014-01-21 14:04:30 -0500

    added multithreading tutorial

---
+++ Tutorial 1 (Multithreading)/src/MyRunnable.java
@@ -0,0 +1,17 @@
+
+// Implementing the Runnable interface, the runnable object is passed to the Thread constructor
+
+public class MyRunnable implements Runnable {
+	
+	public void run() 
+	{
+		System.out.println("This is myRunnable implementing Runnable");
+	}
+	
+	public static void main(String[] args)
+	{
+		new Thread(new MyRunnable()).start();
+		new Thread(new MyRunnable()).start();
+	}
+
+}
